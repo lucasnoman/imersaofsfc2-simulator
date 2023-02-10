4d8cbb5fa45e8ce185b9005759e0e36d9f4bbbe2
Lucas C. Noman <lucas.noman@outlook.com>
2023-02-10 16:02:09 -0300

    Creates back-end api to communicate with Kafka

---
+++ back-end/src/routes/entities/route.entity.ts
@@ -0,0 +1,29 @@
+import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+// Cria um tipo "RouteDocument" que é a junção desses dois que está recebendo
+export type RouteDocument = Route & Document;
+
+@Schema()
+export class Route {
+  @Prop()
+  title: string;
+
+  @Prop(
+    // esse raw define o tipo do campo
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  startPosition: { lat: number; lng: number };
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  endPosition: { lat: number; lng: number };
+}
+
+export const RouteSchema = SchemaFactory.createForClass(Route);
